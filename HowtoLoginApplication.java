65630be70b049d1ac517a983aae52b2f2bdbc662
Ritavik2025 <maisuria4789@gmail.com>
2025-05-17 14:06:20 +1200

    Add files via upload

---
+++ HowtoLoginApplication.java
@@ -0,0 +1,13 @@
+package net.guides.springboot.howtologin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowtoLoginApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowtoLoginApplication.class, args);
+    }
+
+}
